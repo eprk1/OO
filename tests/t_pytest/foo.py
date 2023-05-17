00f217c38a5bfd79e49198fbb941eb572115a83d
Eugene Park <eugene.park@nutanix.com>
2023-05-17 16:04:02 -0400

    pytest

---
+++ tests/t_pytest/foo.py
@@ -0,0 +1,7 @@
+### Mock: mocker.patch
+def load_data():
+    return 1
+
+
+def dummy_func():
+    return load_data()
---
+++ tests/t_pytest/test_pytest.py
@@ -0,0 +1,58 @@
+"""
+$ pytest tests/
+$ pytest tests/test_sample.py
+$ pytest tests/test_sample.py::test_function_one
+
+- All unit tests must be written in a tests/ directory
+- File names should strictly start with test_
+    Otherwise, pytest wont find the test file
+- Function names should strictly start with test
+
+pytest
+pytest-mock
+"""
+import pytest
+import sys
+
+from foo import dummy_func
+
+
+def test_function_with_scenario_one():
+    print("Testing function with scenario one")
+    assert 1 + 1 == 2, f"Check addition value {1 + 1} does not match {2}"
+
+
+def test_mocking_fucntion(mocker):
+    mocker.patch(dummy_func, return_value=2)
+    assert dummy_func() == 2
+
+
+### Parameters
+@pytest.mark.parametrize("n", [2, 4, 6])
+def test_even_number(n):
+    assert not n % 2, f"{n} is not an even number"
+
+
+### Skip
+
+
+@pytest.mark.skip
+def test_skip():
+    raise Exception("This test should be skipped")
+
+
+@pytest.mark.skipif(sys.version_info.major == 2, reason="requires Python 3.x")
+def test_skipif():
+    pass
+
+
+### Fail test
+@pytest.mark.xfail
+def test_fail():
+    assert 1 == 2, "This should fail"
+
+
+### pytest -m <group-name>
+@pytest.mark.group1
+def test_sample():
+    print("test sample")
