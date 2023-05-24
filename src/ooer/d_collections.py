8a0bf420023dff822e57923180248237804b6d24
Eugene Park <eugene.park@nutanix.com>
2023-05-24 16:15:44 -0400

    collection

--- src/ooer/d_collections.py
+++ src/ooer/d_collections.py
@@ -1,4 +1,24 @@
 # in collections we have
 # namedtuple() - factory function for creating tuple with named fields
 # deque - list-like with fast appends and pops from either end
-# chainmap
+# ChainMap - dict like class for creating a single view of multiple mapping
+# Counter - dict subclass for counting hasable objects
+# OrderedDict - dict subclass that remembers the order entries were added
+# defaultdict - dict subclass that calls a factory fucntion to supply missing values
+# UserDict - wrapper around dictionary objects for easier dct subclassing
+# UserList
+# UserString
+
+
+# ChainMap
+from collections import *
+import builtins
+
+
+baseline = {"music": "bach", "art": "rembrandt"}
+adjustments = {"art": "van gogh", "opera": "carmen"}
+cm = list(ChainMap(adjustments, baseline))
+print(cm)
+
+pylookup = ChainMap(locals(), globals(), vars(builtins))
+print(pylookup)
